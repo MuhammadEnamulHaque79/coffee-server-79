ed0f95b0764a6448242dfa20cbdc070159c9ce8c
Muhammad Enamul Haque <muhammad.enamul.haque79@gmail.com>
2023-05-15 00:59:30 +0600

    added

---
+++ __Steps.js
@@ -0,0 +1,12 @@
+/*
+............Which dependency you install.........
+1.npm init;
+2.npm install express;
+3.cors;{app.use(cors())}
+4.app.use(express.json())
+5.npm install dotenv --save;
+6.mongoDB;
+
+
+
+*/
